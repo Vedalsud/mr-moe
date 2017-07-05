b35451acdfcb4c4d603ceb976b7311e955589c82
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 17:48:25 +0000

    integrated database relationship between topic and blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170629143728) do
-
+ActiveRecord::Schema.define(version: 20170705172347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +18,10 @@ ActiveRecord::Schema.define(version: 20170629143728) do
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
+    t.integer  "status",     default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
@@ -48,21 +47,17 @@ ActiveRecord::Schema.define(version: 20170629143728) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "portfolios", force: :cascade do |t|
-    t.string   "title"
-    t.string   "subtitle"
-    t.text     "body"
-    t.text     "main_image"
-    t.text     "thumb_image"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "skills", force: :cascade do |t|
     t.string   "title"
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
